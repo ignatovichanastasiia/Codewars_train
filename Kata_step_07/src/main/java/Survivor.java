5d9f1717b563dc265d95167fff0d5f58a64fb4c3
Tigg <ignatovichanastasiia@gmail.com>
2021-12-20 23:36:00 +0300

    add counter for winner(+best work from site for understand (in comments))

--- Kata_step_07/src/main/java/Survivor.java
+++ Kata_step_07/src/main/java/Survivor.java
@@ -34,3 +34,14 @@ public class Survivor {
         return list.get(0);
     }
 }
+
+//import java.util.Arrays;
+//
+//public class JosephusSurvivor {
+//    public static int josephusSurvivor(final int n, final int k) {
+//        if(n == 1)
+//            return 1;
+//
+//        return (josephusSurvivor(n - 1, k) + k - 1) % n + 1;
+//    }
+//}
